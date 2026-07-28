01385b0868f16e0c219590b6d8e87e2563340f64
lucky760081-gif <lucky760081@gmail.com>
2026-07-28 21:39:29 +0530

    Time: 896 ms (5.88%) | Memory: 13.9 MB (41.18%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def smallestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        cnt = Counter(s)
+
+        left = []
+        mid = ""
+
+        for c in sorted(cnt):
+            left.append(c * (cnt[c] // 2))
+            if cnt[c] % 2:
+                mid = c
+
+        left = "".join(left)
+        return left + mid + left[::-1]
