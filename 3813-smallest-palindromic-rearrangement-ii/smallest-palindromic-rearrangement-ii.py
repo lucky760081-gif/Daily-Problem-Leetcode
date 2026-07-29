926ff4172f02672211568e13c35e8e7913ab8568
lucky760081-gif <lucky760081@gmail.com>
2026-07-29 15:39:31 +0530

    Time: 943 ms (0.00%) | Memory: 12.9 MB (0.00%) - LeetSync

--- 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.py
+++ 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.py
@@ -1,52 +1,32 @@
-class Solution:
-    def smallestPalindrome(self, s: str, k: int) -> str:
-        LIMIT = 10**6 + 1
+from collections import Counter
+class Solution(object):
+    def smallestPalindrome(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        m = len(s) >> 1
+        freq = Counter(s[:m])
 
-        freq = Counter(s)
+        perm = factorial(m)
+        for v in freq.values():
+            perm //= factorial(v)
 
-        mid = ""
-        half = [0] * 26
+        if k > perm: return ""
 
-        for ch, cnt in freq.items():
-            if cnt & 1:
-                mid = ch
-            half[ord(ch) - 97] = cnt // 2
+        half = ""
+        for i in range(m):
+            for c in ascii_lowercase:
+                if not freq[c]: continue
 
-        def count_perm(cnts):
-            total = sum(cnts)
-            res = 1
-            rem = total
-
-            for c in cnts:
-                if c:
-                    res *= comb(rem, c)
-                    if res >= LIMIT:
-                        return LIMIT
-                    rem -= c
-
-            return res
-
-        total_perm = count_perm(half)
-        if total_perm < k:
-            return ""
-
-        left = []
-        m = sum(half)
-
-        for _ in range(m):
-            for i in range(26):
-                if half[i] == 0:
-                    continue
-
-                half[i] -= 1
-                ways = count_perm(half)
-
-                if ways >= k:
-                    left.append(chr(i + 97))
+                t = perm * freq[c] // (m - i)
+                if k <= t:
+                    freq[c] -= 1
+                    half += c
+                    perm = t
                     break
+                k -= t
 
-                k -= ways
-                half[i] += 1
-
-        left = "".join(left)
-        return left + mid + left[::-1]
+        mid = s[m] if len(s) & 1 else ""
+        return half + mid + half[::-1]
