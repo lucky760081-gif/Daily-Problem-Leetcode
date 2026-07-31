9b717ab43242757f03499a1f7848cafdc85f6590
lucky760081-gif <lucky760081@gmail.com>
2026-07-31 21:16:32 +0530

    Time: 409 ms (50.00%) | Memory: 12.8 MB (41.67%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minimumPushes(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        freq = sorted(Counter(word).values(), reverse=True)
+
+        ans = 0
+        for i, f in enumerate(freq):
+            ans += f * (i // 8 + 1)
+
+        return ans
